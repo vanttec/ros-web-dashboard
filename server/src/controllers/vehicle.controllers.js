f7e3491c1c99aafe0ff59ff08f1f1d9863d6689a
Robertomendivil97 <robertomc97@gmail.com>
2022-05-28 21:19:34 -0500

    updated controllers

--- server/src/controllers/vehicle.controllers.js
+++ server/src/controllers/vehicle.controllers.js
@@ -6,12 +6,12 @@ const getVehicles = async (req, res) => {
 };
 
 const getVehicleById = async (req, res) => {
-    const task = await boatTask.findById(req.params.id); 
+    const task = await Task.findById(req.params.id); 
     return res.json(task);
 };
 
 const createVehicle = async (req, res) => {
-    const task = new boatTask(req.body);
+    const task = new Task(req.body);
     await task.save();
     res.json({
         status: 'Data saved'
@@ -19,14 +19,14 @@ const createVehicle = async (req, res) => {
 }
 
 const updateVehicle = async (req, res) => {
-    await boatTask.findByIdAndUpdate(req.params.id, req.body);
+    await Task.findByIdAndUpdate(req.params.id, req.body);
     res.json({
         status: 'Data updated'
     })
 }
 
 const deleteVehicle = async (req, res) => {
-    await boatTask.findByIdAndRemove(req.params.id);
+    await Task.findByIdAndRemove(req.params.id);
     res.json({
         status: 'Data removed'
     })
