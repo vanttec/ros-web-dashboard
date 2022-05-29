9bd120d363c5da892ded9e9f5d5288bdeaf16b72
Sebastian Martínez <55563384+vSebas@users.noreply.github.com>
2022-05-28 21:56:17 -0500

    updated

--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,27 @@
 import 'leaflet/dist/leaflet.css';
 
-import Vue from 'vue'
-// import App from './App.vue'
-import App from './app/components/map.vue'
-import { Icon } from 'leaflet';
+import { createApp } from 'vue'
+// import App from './app/components/map.vue'
+import App from './mapApp.vue'
+
+// import { Icon } from 'leaflet';
+import OpenLayersMap from 'vue3-openlayers'
+import 'vue3-openlayers/dist/vue3-openlayers.css'
 
 Vue.config.productionTip = false
-new Vue({
-  render: h => h(App),
-}).$mount('#map')
 
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+// createApp(App).$mount('#app')
+
+const app = createApp(App);
+app.use(OpenLayersMap)
+
+app.mount('#app')
+
+
+// delete Icon.Default.prototype._getIconUrl;
+
+// Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+// });
