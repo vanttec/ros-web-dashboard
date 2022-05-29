eef6101cdc9f2ae5b4f0059319e8da490e2b1d9d
Robertomendivil97 <robertomc97@gmail.com>
2022-05-28 20:46:51 -0500

    mongoose collection updated

--- server/src/models/Tasks.js
+++ server/src/models/Tasks.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
 const taskSchemaVehicle = new mongoose.Schema({
-    _id: String,
     _vehicle_name:String,
     _description: String,
-    _mechanical_dimmensions: Array,
+    _dimensions: Array,
     _mass: String,
     _thruster_name: String,
-    _max_thrust: Object,
+    _max_thrust: String,
     _sensors:Array,
-    _Power_System: String,
-    _Processor: String,
+    _power_system: String,
+    _processor: String,
     _communications: Array,
     _software: Array
 });
 
-module.exports = mongoose.model('Vehicle_Task', taskSchemaVehicle)
+module.exports = mongoose.model('Vehicle', taskSchemaVehicle)
