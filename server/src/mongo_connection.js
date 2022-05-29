751570c7fb7b8ac13c50f80c43b7cf9b12448702
Robertomendivil97 <robertomc97@gmail.com>
2022-05-28 20:47:36 -0500

    get vehicle on port 3000

--- server/src/mongo_connection.js
+++ server/src/mongo_connection.js
@@ -2,7 +2,7 @@
 var connect = require('mongoose').connect;
 (async () => {
     try {
-        const db = await connect('mongodb+srv://vanttec-admin:vanttec@vtec.cnsa8.mongodb.net/vanttec_db');
+        const db = await connect('mongodb+srv://vanttec-admin:vanttec@vtec.cnsa8.mongodb.net/vanttec_db/?retryWrites=true&w=majority');
         console.log("Atlas DB connected to", db.connection.name);
     } catch (error) {
         console.error(error)
